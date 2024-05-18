91548b0d9144bec3a47c24465ac4c61d6a6a74ea
Estavio <wash63431@gmail.com>
2024-05-18 16:56:31 +0800

    Scuffed Hotbar

--- JameGamUPLB/Scenes/character.gd
+++ JameGamUPLB/Scenes/character.gd
@@ -5,8 +5,22 @@ const JUMP_SPD: int = -1800
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
-	if Input.is_action_pressed("Slash"):
-		$AnimatedSprite2D/AnimatedSprite2D2.play("default")
+	if Input.is_action_pressed("EquipS"):
+		if Input.is_action_pressed("MainAction"):
+			$AnimatedSprite2D/AnimatedSprite2D2.play("default")
+		elif Input.is_action_pressed("SecondaryAction"):
+			$AnimatedSprite2D/AnimatedSprite2D2.play("thrust")
+	if Input.is_action_pressed("EquipSeed"):
+		if Input.is_action_pressed("MainAction"):
+			$AnimatedSprite2D/AnimatedSprite2D3.play("grow")
+		elif Input.is_action_pressed("SecondaryAction"):
+			$AnimatedSprite2D/AnimatedSprite2D3.play("grow")
+	if Input.is_action_pressed("EquipW"):
+		if Input.is_action_pressed("MainAction"):
+			$AnimatedSprite2D/AnimatedSprite2D.play("Water")
+		elif Input.is_action_pressed("SecondaryAction"):
+			$AnimatedSprite2D/AnimatedSprite2D.play("Water")
+	
 	if is_on_floor():
 		$CollisionShape2D.disabled=false
 		if Input.is_action_pressed("Up"):
